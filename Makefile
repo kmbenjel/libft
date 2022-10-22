50371e1a5f8af01af842a182cc27d85d6f6814a0
Khalid Benjelloun <kbenjell@student.1337.ma>
2022-10-22 13:23:12 +0100

    kbenjell

---
+++ Makefile
@@ -0,0 +1,5 @@
+Name
+all
+clean
+fclean
+re
---
+++ libft.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 20:07:41 by kbenjell          #+#    #+#             */
+/*   Updated: 2022/10/22 13:04:49 by kbenjell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct a_list
+{
+	void *content;
+	struct a_list *next;
+
+} t_list;
+
+
+/*
+isalnum
+isalpha
+isdigit
+isascii
+isprint
+strlen
+memset
+bzero
+memcpy
+memove
+strlcpy
+strlcat
+toupper
+tolower
+strchr
+strrch
+strncmp
+memchr
+strnstr
+atoi
+calloc
+strdup
+substr
+strjoin
+strtrim
+split
+itoa
+strmapi
+striteri
+putchar_fd
+putstr_fd
+putnbr_fd
+lstnew
+lstadd_front
+lstsize
+lstlast
+lstadd_back
+lstdelone
+lstclear
+lstiter
+lstmap
+
+
+*/
