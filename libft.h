dc2feb1f66a083874546a0e439be7653705797a2
Khalid Benjelloun <kbenjell@student.1337.ma>
2022-10-24 10:23:49 +0100

    kbenjell

--- libft.h
+++ libft.h
@@ -6,60 +6,56 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 20:07:41 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/10/22 13:04:49 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/10/22 17:24:23 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 typedef struct a_list
 {
-	void *content;
-	struct a_list *next;
+	void			*content;
+	struct a_list	*next;
 
-} t_list;
+}					t_list;
 
-
-/*
-isalnum
-isalpha
-isdigit
-isascii
-isprint
-strlen
-memset
-bzero
-memcpy
-memove
-strlcpy
-strlcat
-toupper
-tolower
-strchr
-strrch
-strncmp
-memchr
-strnstr
-atoi
-calloc
-strdup
-substr
-strjoin
-strtrim
-split
-itoa
-strmapi
-striteri
-putchar_fd
-putstr_fd
-putnbr_fd
-lstnew
-lstadd_front
-lstsize
-lstlast
-lstadd_back
-lstdelone
-lstclear
-lstiter
-lstmap
-
-
-*/
+int					ft_isalnum(int c);
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_strlen(int c);
+int					ft_memset(int c);
+int					ft_bzero(int c);
+int					ft_memcpy(int c);
+int					ft_memove(int c);
+int					ft_strlcpy(int c);
+int					ft_strlcat(int c);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+int					ft_strchr(int c);
+int					ft_strrch(int c);
+int					ft_strncmp(int c);
+int					ft_memchr(int c);
+int					ft_strnstr(int c);
+int					ft_atoi(int c);
+int					ft_calloc(int c);
+int					ft_strdup(int c);
+int					ft_substr(int c);
+int					ft_strjoin(int c);
+int					ft_strtrim(int c);
+int					ft_split(int c);
+int					ft_itoa(int c);
+int					ft_strmapi(int c);
+int					ft_striteri(int c);
+int					ft_putchar_fd(int c);
+int					ft_putstr_fd(int c);
+int					ft_putnbr_fd(int c);
+int					ft_lstnew(int c);
+int					ft_lstadd_front(int c);
+int					ft_lstsize(int c);
+int					ft_lstlast(int c);
+int					ft_lstadd_back(int c);
+int					ft_lstdelone(int c);
+int					ft_lstclear(int c);
+int					ft_lstiter(int c);
+int					ft_lstmap(int c);
