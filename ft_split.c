7beba70d00641e14ed6b72db95a8ff65be8a1083
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-05 23:02:29 +0100

    kbenjell

---
+++ ft_split.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 20:28:36 by kbenjell          #+#    #+#             */
+/*   Updated: 2022/11/05 23:02:26 by kbenjell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	splits_count(char const *s, char c)
+{
+	int	splits;
+	int	i;
+
+	splits = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			i++;
+		}
+		else
+		{
+			splits++;
+			while (s[i] != c)
+				i++;
+		}
+	}
+	return (splits);
+}
+
+int	*splits_charcount(char const *s, char c, int sc)
+{
+	int	i;
+	int	j;
+	int	*counts;
+
+	i = 0;
+	j = 0;
+	counts = (int *)malloc((sc + 1) * sizeof(int));
+	if (!counts)
+		return (0);
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			i++;
+		}
+		else
+		{
+			while (s[i] != c)
+			{
+				counts[j]++;
+				i++;
+			}
+			j++;
+		}
+	}
+	counts[j] = 0;
+	return (counts);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**splits;
+	int		*counts;
+	int		sc;
+	int		i;
+
+	sc = splits_count(s, c);
+	counts = spllits_charcount(s, c, sc);
+	i = 0;
+	splits = (char **)malloc((sc + 1) * sizeof(char *));
+	if (!splits)
+		return (0);
+	while (i < sc)
+	{
+		splits[i] = (char *)malloc((counts[i] + 1) * sizeof(char));
+		i++;
+	}
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			i++;
+		}
+		else
+		{
+			while (s[i] != c)
+			{
+				splits[j][i] == s[i];
+				i++;
+			}
+			j++;
+		}
+	}
+	return (splits);
+}
