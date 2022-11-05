8976914a80df5a95aec95fe9eabf63fff7ba47de
Khalid Benjelloun <kbenjell@student.1337.ma>
2022-11-05 07:28:33 +0100

    x

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,24 +1,24 @@
-size_t ft_strlen(const char *str)
-{
-	unsigned int c;
+// size_t ft_strlen(const char *str)
+// {
+// 	unsigned int c;
 
-	c = 0;
-	while (str[c])
-		c++;
-	return c;
-}
+// 	c = 0;
+// 	while (str[c])
+// 		c++;
+// 	return (c);
+// }
 
-size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
+// size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
+// {
 
-	unsigned int i;
+// 	unsigned int i;
 
-	i = 0;
-	while (i < dstsize - 1)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = 0;
-	return ft_strlen(src);
-}
+// 	i = 0;
+// 	while (i < dstsize - 1)
+// 	{
+// 		dst[i] = src[i];
+// 		i++;
+// 	}
+// 	dst[i] = 0;
+// 	return (ft_strlen(src));
+// }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,19 +6,19 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 21:39:02 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/03 21:39:04 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/05 07:27:56 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*trimmed;
+// #include "libft.h"
+// char	*ft_strtrim(char const *s1, char const *set)
+// {
+// 	char	*trimmed;
 
-	trimmed = (char *)malloc(ft_strlen(s1) + 1);
-	if (!trimmed)
-		return (0);
-	if ()
+// 	trimmed = (char *)malloc(ft_strlen(s1) + 1);
+// 	if (!trimmed)
+// 		return (0);
+// 	if ()
 
-	return (trimmed);
-}
+// 	return (trimmed);
+// }
