b4af2724e450275a38cdb6a760e864ce0688f273
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-10 12:18:29 +0100

    kbenjell

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,11 +6,12 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 21:11:22 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/05 06:41:55 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/10 12:18:25 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 size_t	ft_strlen(const char *str)
 {
 	unsigned int	c;
