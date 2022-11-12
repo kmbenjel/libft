d404baa22392e78bcb64fb262d539267d87685ae
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-12 11:17:05 +0100

    kbenjell

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,9 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 20:07:55 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/12 06:32:10 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/12 11:15:07 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+void *ft_memset(void *b, int c, size_t len);
