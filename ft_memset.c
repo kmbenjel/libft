5b762b72cce937ea9ad5a465d1daeb53828f7ee7
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-12 17:35:20 +0100

    kbenjell

--- ft_memset.c
+++ ft_memset.c
@@ -6,19 +6,17 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 20:07:55 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/12 16:07:47 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/12 17:34:37 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t max;
+	unsigned char	*p;
 
-	max = len;
-	while (--len)
-	{
-		(char)b[max - len] = (unsigned char)c;
-	}
-	return (b);
+	p = b;
+	while (len--)
+		*p = (unsigned char)c;
+	return (p);
 }
