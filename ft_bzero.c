31d680b569fd50b23c07e841a697f090bd68ad6f
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-14 19:43:45 +0100

    kbenjell

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,10 +6,17 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 00:39:53 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/14 18:39:53 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/14 19:43:35 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 void	ft_bzero(void *s, size_t n)
 {
+	char	*p;
+
+	p = s;
+	while (n--)
+		*p = 0;
 }
