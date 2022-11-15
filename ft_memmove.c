afb9be9d121862f63539ed2ec1f28bdc027e3029
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-15 11:28:35 +0100

    kbenjell

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,17 +6,20 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 23:48:20 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/15 11:20:56 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/15 11:28:32 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
+
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*p;
 
 	p = dst;
+	if (!dst && !src)
+		return (0);
 	while (len--)
-		if (src)
+		if (!(p + len))
 			*(p + len) = *((char *)src + len);
 	return (dst);
 }
