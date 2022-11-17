42828922ab9af1a27bd049a8800ab2052e396c18
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-17 08:14:53 +0100

    kbenjell

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,24 +6,35 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 23:48:20 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/17 06:38:07 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/17 08:10:01 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*p;
-	size_t	n;
-	char	*tmp;
+	char	*d;
+	char	*s;
+	char	tmp;
 
-	tmp = ft_memset(tmp, 'c', len);
 	p = dst;
-	n = len;
+	s = (char *)src;
+	tmp = 'i';
 	if (!src && !dst)
 		return (0);
-	if (len = 0)
-		return (dst) while (--n) if () * (p + n) = *((char *)src + n);
+	if (len == 0)
+		return (dst);
+	while (n--)
+	{
+		if (stc < dest)
+		{
+			tmp = src[n];
+			p[n] = (char *)src[n];
+			src[n] = tmp;
+		}
+		else
+			p[n] = (char *)src[n];
+	}
 	return (dst);
 }
 
