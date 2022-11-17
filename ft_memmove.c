f1a4f52130a1108354bcdfe1a26ae212a6347f71
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-17 09:29:42 +0100

    kbenjell

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,47 +6,44 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 23:48:20 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/17 08:22:33 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/17 09:29:22 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+int restrict	ft_step_between(char *d, char *s)
+{
+	int	step;
+
+	step = 0;
+	while (s)
+	{
+		
+	}
+}
+*/
+
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*d;
 	char	*s;
-	char	tmp;
+	size_t	n;
 
+	n = len;
 	d = dst;
 	s = (char *)src;
-	tmp = 'i';
 	if (!src && !dst)
 		return (0);
 	if (len == 0)
 		return (dst);
-	while (len--)
+	if (dst < src)
 	{
-		if (src < dst)
-		{
-			tmp = *((char *)src + len);
-			d[len] = *((char *)src + len);
-			*((char *)src + len) = tmp;
-		}
-		else
-			d[len] = *((char *)src + len);
+		while (n--)
+			*(d + len - n - 1) = *(s + len - n - 1);
 	}
+	else
+		ft_memcpy(dst, src, len);
 	return (dst);
 }
-
-/*
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
