2154253e3e937025241794f963595a6716841648
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-17 21:13:21 +0100

    kbenjell

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,32 +6,22 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 07:48:04 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/12 06:19:27 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/17 21:11:26 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// size_t ft_strlen(const char *str)
-// {
-// 	unsigned int c;
+#include "libft.h"
 
-// 	c = 0;
-// 	while (str[c])
-// 		c++;
-// 	return (c);
-// }
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	unsigned int	i;
 
-// size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
-// {
-
-// 	unsigned int i;
-
-// 	i = 0;
-// 	while (i < dstsize - 1)
-// 	{
-// 		dst[i] = src[i];
-// 		i++;
-// 	}
-// 	dst[i] = 0;
-// 	return (ft_strlen(src));
-// }
-//kahlx
+	i = 0;
+	while (i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = 0;
+	return (ft_strlen(src));
+}
