0e6c1a0a19ffec2bf8d7ee916c46569f5dd77b76
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-18 22:15:31 +0100

    kbenjell

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,11 +6,11 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 20:38:23 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/18 21:56:20 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/18 22:11:12 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
+
 char	*ft_strchr(const char *s, int c)
 {
 	char	*string;
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,8 +6,25 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 20:42:15 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/18 20:49:08 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/18 22:11:49 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-char	*strrchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*string;
+	char	character;
+
+	string = (char *)s;
+	character = (char)c;
+	while (*string)
+	{
+		if (*string == character)
+			return (string);
+		string++;
+	}
+	if (character == 0)
+		return (string);
+	return (0);
+}
