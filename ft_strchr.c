fad1d521f1536411d27ff806682cbf01ea900a21
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-18 21:27:25 +0100

    kbenjell

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 20:38:23 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/18 21:20:11 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/18 21:27:02 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ char	*ft_strchr(const char *s, int c)
 	char	*string;
 
 	string = (char *)s;
+	(void)c;
+	*string = '\0';
+	/*string = (char *)s;
 	while (*string)
 	{
 		if (*string == c)
@@ -23,6 +26,6 @@ char	*ft_strchr(const char *s, int c)
 		string++;
 	}
 	if (c == 0)
-		return (string);
-	return (0);
+		return (string);*/
+	return (string);
 }
