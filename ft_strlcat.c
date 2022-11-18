f26454c62f6160f43375b5e633690d02f18fb162
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-18 17:02:24 +0100

    kbenjell

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 00:40:00 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/18 16:43:13 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/18 17:02:10 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,24 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 	size_t	j;
 	size_t	srcsize;
+	size_t	lendst;
 
-	i = -1;
+	i = 0;
 	j = 0;
+	lendst = ft_strlen(dst);
 	srcsize = ft_strlen(src);
-	if (dstsize > 0 && i < dstsize + srcsize && srcsize)
+	if (dstsize > 0 && i <= dstsize + srcsize && srcsize)
 	{
-		while (++i < srcsize - 1)
-			dst[i] = src[i];
-		while (++i < srcsize + dstsize)
+		while (i < lendst && i < dstsize)
+			i++;
+		while (i < dstsize - 1)
 		{
 			dst[i] = src[j];
+			i++;
 			j++;
 		}
+		dst[i] = '\0';
+		i++;
 	}
-	return (srcsize + dstsize);
+	return (i);
 }
