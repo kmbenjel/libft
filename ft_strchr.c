2f77dbf4a40870d522f2e9831b9baf61eb74bf02
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-18 21:14:16 +0100

    kbenjell

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,20 +6,23 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 20:38:23 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/18 20:58:00 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/18 21:13:31 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
+	char	*string;
+
+	string = (char *)s;
+	while (*string)
 	{
-		if (c == '\0' && *(s + 1) == '\0')
-			return (*(s + 1));
-		if (*s == c)
-			return (s);
-		s++;
+		if (*string == c)
+			return (string);
+		string++;
 	}
+	if (c == 0)
+		return (string);
 	return (0);
 }
