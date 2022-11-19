526935ad37d493b234eaf31dc247c197f35be1c3
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-19 09:18:01 +0100

    kbenjell

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 22:37:49 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/17 22:41:22 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/19 09:17:27 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	unsigned int	i;
 
 	i = 0;
+	if (ft_strlen(needle) == 0)
+		return ((char *)haystack);
 	while (haystack[i] && i <= len - ft_strlen(needle))
 	{
-		if ((ft_strncmp(&haystack[i], needle, ft_strlen(needle))) == 0)
+		if ((ft_strncmp(&haystack[i], needle, ft_strlen(needle))) == 0
+			&& ft_strlen(needle) <= len)
 			return ((char *)&haystack[i]);
 		i++;
 	}
