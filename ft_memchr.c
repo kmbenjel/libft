0375c51927d5e89f48ef82a96e57e2e7aad01aa9
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-19 08:03:31 +0100

    kbenjell

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 22:27:20 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/18 22:53:54 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/19 08:00:57 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,15 +17,13 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	character;
 
 	string = (unsigned char *)s;
-	character = (unsigned char)c;
-	while (n && *string)
+	character = c;
+	while (n)
 	{
 		if (*string == character)
 			return (string);
 		string++;
 		n--;
 	}
-	if (character == 0 && n > 0)
-		return (string);
 	return (0);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 21:39:09 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/17 22:42:16 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/19 08:02:11 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*one;
+	unsigned char	*two;
 
 	i = 0;
+	one = (unsigned char *)s1;
+	two = (unsigned char *)s2;
 	while ((s1[i] || s2[i]) && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (one[i] != two[i])
+			return (one[i] - two[i]);
 		i++;
 	}
 	return (0);
