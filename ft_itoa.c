cd8da45407025a890489f27a66d828f7fbbae77d
kmbenjel <khalidmailing@gmail.com>
2022-11-22 03:29:32 +0100

    Recent edit

--- ft_itoa.c
+++ ft_itoa.c
@@ -17,10 +17,14 @@ char *ft_itoa(int n)
 	char *str;
 	unsigned int target;
 	unsigned int i;
+	int int_min;
 
+	int_min = -2147483648;
 	target = ft_count_target(n);
 	str = malloc(ft_count_target(n));
 
+	if (n == int_min)
+		return "-2147483648";
 	if (n < 0) {
 		*str = '-';
 		n *= -1;
@@ -28,14 +32,10 @@ char *ft_itoa(int n)
 	str[target - 1] = 0;
 	str += target - 1;
 	i = target - 1;
-	while (n)
+	while (n && str-- && i--)
 	{
-		i--;
-		str--;
 		*str = n % 10 + '0';
 		n /= 10;
 	}
-	if (i == 1)
-		str--;
-	return str;
+	return str - i;
 }
--- test__ft_itoa.c
+++ test__ft_itoa.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "ft_itoa.c"
 #include <stdlib.h>
+#include <limits.h>
 
 int main(int ac, char**av)
 {
 	int n = atoi(av[1]);
-	printf("%s", ft_itoa(n));
+	printf("%s", ft_itoa(INT_MIN));
 }
