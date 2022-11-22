a829f410d18f194eff5a1111babf6d8c298b1bb9
kmbenjel <khalidmailing@gmail.com>
2022-11-22 01:57:56 +0100

    Recent edit

---
+++ ft_itoa.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int static ft_count_target(int n)
+{
+	int i;
+
+	i = 0;
+	while (i++)
+		n/=10;
+	return i;
+}
+
+// char *ft_itoa(int n)
+// {
+// 	char *str;
+
+// 	malloc =
+// }
---
+++ test__ft_itoa.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "ft_itoa.c"
+#include <stdlib.h>
+
+int main(int ac, char**av)
+{
+	int n = atoi(av[1]);
+	printf("%d", ft_count_target(n));
+}
