8a6dfe7d09e6d0c2545ddd0697eab10b8b7db3ed
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-23 08:30:09 +0100

    kbenjell

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 07:29:08 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/23 07:29:19 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/23 08:29:30 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -16,8 +16,12 @@ unsigned int static	ft_count_target(int n)
 	unsigned int	i;
 
 	i = 1;
+	if (n == -2147483648)
+		return (ft_count_target(n / 10) + 1);
 	if (n < 0)
 		i++;
+	if (n == 0)
+		return (2);
 	while (n && i++)
 		n /= 10;
 	return (i);
@@ -35,6 +39,8 @@ char	*ft_itoa(int n)
 
 	target = ft_count_target(n);
 	str = malloc(target);
+	if (!str)
+		return (0);
 	if (n == -2147483648)
 	{
 		str = "-2147483648";
@@ -45,13 +51,18 @@ char	*ft_itoa(int n)
 		*str = '-';
 		n *= -1;
 	}
-	str[target - 1] = 0;
+	str[target - 1] = '\0';
 	str += target - 1;
 	i = target - 1;
-	while (n && str-- && i--)
+	if (!n)
 	{
-		*str = n % 10 + '0';
-		n /= 10;
+		*(str--) = '0';
 	}
+	else
+		while (n && str-- && i--)
+		{
+			*str = n % 10 + '0';
+			n /= 10;
+		}
 	return (str - i);
 }
