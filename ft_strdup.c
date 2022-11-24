fc2afac2f3508b8f9a60aae4d88e2b504e0f62ef
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-24 20:58:47 +0100

    kbenjell

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,15 +6,17 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 20:39:58 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/24 20:50:06 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/24 20:57:14 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
 	char	*copy;
 
 	copy = malloc(ft_strlen(s1) + 1);
-	return (ft_memcpy(copy, s1, ft_strlen(s1)));
+	if (!copy)
+		return (0);
+	return (ft_memcpy(copy, s1, ft_strlen(s1) + 1));
 }
