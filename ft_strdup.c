fa027eddbfd8f0ffda4cb1af868b6eff54637cf1
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-24 20:50:18 +0100

    kbenjell

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,6 +6,15 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 20:39:58 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/24 20:40:16 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/24 20:50:06 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+char	*strdup(const char *s1)
+{
+	char	*copy;
+
+	copy = malloc(ft_strlen(s1) + 1);
+	return (ft_memcpy(copy, s1, ft_strlen(s1)));
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 20:07:41 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/23 10:45:56 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/24 20:46:13 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ char				*ft_strnstr(const char *haystack, const char *needle,
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 int					ft_atoi(const char *str);
 void				*ft_calloc(size_t count, size_t size);
-//int					ft_strdup(int c);
+char				*ft_strdup(const char *s1);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
