fa6cd4a88c54aa46d419d76452e6f4bd09d9c989
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-24 21:04:52 +0100

    kbenjell

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,16 +6,17 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 21:39:02 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/13 23:35:06 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/24 21:04:50 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// char	*ft_strtrim(char const *s1, char const *set)
-// {
-// 	char	*trimmed;
-// 	trimmed = (char *)malloc(ft_strlen(s1) + 1);
-// 	if (!trimmed)
-// 		return (0);
-// 	if ()
-// 	return (trimmed);
-// }
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trimmed;
+
+	trimmed = (char *)malloc(ft_strlen(s1) + 1);
+	if (!trimmed)
+		return (0);
+	if ()
+		return (trimmed);
+}
