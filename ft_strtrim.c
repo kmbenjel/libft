4be5373134452263f86e359eeedf3a0460397e50
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-25 09:19:27 +0100

    kbenjell

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 21:39:02 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/25 08:28:40 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/25 09:17:41 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -21,17 +21,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	times = 0;
 	start = 0;
-	if (!s1 || ft_strncmp(s1, set, ft_strlen(s1)))
+	if (!s1)
 		return (0);
 	if (!set)
 		return (ft_strdup(s1));
 	last = (char *)s1 + ft_strlen(s1) - ft_strlen(set);
-	if (ft_strnstr(s1, set, ft_strlen(set)) == (char *)s1)
-	{
-		times++;
+	if (ft_strnstr(s1, set, ft_strlen(set)) == (char *)s1 && !times++)
 		start += ft_strlen(set);
-	}
-	if (ft_strnstr(last, set, ft_strlen(set)) == last)
+	if (ft_strnstr(last, set, ft_strlen(set)) == last && last != (char *)s1)
 		times++;
 	if (times == 0)
 		return (ft_strdup(s1));
@@ -40,6 +37,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	trimmed = ft_strdup(trimmed + start);
 	if (!trimmed)
 		return (0);
-	trimmed[lresult] = 0;
+	//if (lresult)
+	//	trimmed[lresult] = 0;
 	return (trimmed);
 }
