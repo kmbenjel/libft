932de361f76b19550316e5f4c0b2e8699edb904b
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-27 00:56:38 +0100

    kbenjell

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 03:43:01 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/27 00:53:25 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/27 00:56:07 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,14 +18,15 @@ void	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 
 	p = malloc(ft_strlen(s) + 1);
-	ft_strlcpy(p, s, ft_strlen(s) + 1);
 	if (!p)
 		return (0);
+	ft_strlcpy(p, s, ft_strlen(s) + 1);
 	i = 0;
 	while (*(p + i))
 	{
 		f(i, *(p + i));
 		i++;
 	}
-	return (f(i, *p));
+	*(p + i) = 0;
+	return (p);
 }
