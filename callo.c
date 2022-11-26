3ff243be1c2720c1aa2a4f2abbd128eabc80abd5
Khalid Benjelloun <khalidmailing@gmail.com>
2022-11-26 02:58:47 +0100

    kbenjell

---
+++ callo.c
@@ -0,0 +1,13 @@
+
+#include "libft.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	printf("%zu\n", strlen(""));
+	return (0);
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,34 +6,66 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 21:39:02 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/11/25 10:44:53 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/11/25 12:39:32 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+static int	isfromset(char const *set, char c)
+{
+	int	i;
+
+	i = 0;
+	while (*(set + i))
+	{
+		if (c == *(set + i))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	calc_start(char const *set, char *p)
+{
+	unsigned int	start;
+
+	start = 0;
+	while (isfromset(set, *(p + start)))
+		start++;
+	return (start);
+}
+
+static int	calc_end(char const *set, char *p)
+{
+	unsigned int	end;
+
+	end = 0;
+	while (isfromset(set, *(p - end)))
+		end++;
+	return (end);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char			*trimmed;
+	char			*p;
 	unsigned int	start;
 	unsigned int	end;
 	unsigned int	lresult;
-	unsigned int	lset;
 
-	lset = ft_strlen(set);
-	start = 0;
-	end = ft_strlen(s1);
-	lresult = end;
-	if (!s1)
+	p = (char *)s1;
+	if (!p || !set)
 		return (0);
-	if (!set)
-		return (ft_strdup(s1));
-	while (ft_strnstr(s1 + start, set, lset) == s1 + start)
-		start += lset;
-	while (ft_strnstr(s1 + lresult - end, set, lset) == s1 + lresult - end)
-		end -= lset;
-	lresult = end - start;
+	if (!ft_strlen(set) || !ft_strlen(p))
+		return (ft_strdup(p));
+	start = calc_start(set, p);
+	end = calc_end(set, p + ft_strlen(p) - 1);
+	if (start == ft_strlen(p))
+		end = 0;
+	lresult = ft_strlen(p) - (start + end);
 	trimmed = malloc(lresult + 1);
-	ft_strlcpy(trimmed, s1 + start, lresult + 1);
-	trimmed[lresult] = 0;
+	if (!trimmed)
+		return (0);
+	ft_strlcpy(trimmed, p + start, lresult + 1);
 	return (trimmed);
 }
