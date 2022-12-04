154e3f312fde5e8820f69fc53aefbe2308a6a9d8
Khalid Benjelloun <khalidmailing@gmail.com>
2022-12-04 20:27:32 +0100

    kbenjell

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,12 +6,28 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 18:16:11 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/12/04 17:00:14 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/12/04 20:25:44 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		if (n == INT_MIN)
+		{
+			ft_putchar_fd('2', fd);
+			n = 147483648;
+			//ft_putnbr_fd(147483648, fd);
+		}
+		else
+			n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd(n % 10 + '0', fd);
 }
--- test__ft_putnbr_fd.c
+++ test__ft_putnbr_fd.c
@@ -1,10 +1,10 @@
 #include "ft_itoa.c"
+#include "ft_putchar_fd.c"
 #include "ft_putnbr_fd.c"
 #include "ft_putstr_fd.c"
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 int	main(int ac, char **av)
 {
 	int	n;
