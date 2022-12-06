b19ef8b0ce94611a346f0c906e8b56a94ab7ffca
Khalid Benjelloun <khalidmailing@gmail.com>
2022-12-06 22:03:24 +0100

    kbenjell

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 21:59:13 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/12/06 21:15:01 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/12/06 22:03:21 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,6 +15,6 @@ t_list	*ft_lstnew(void *content)
 {
 	if (!content)
 		return (0);
-	content = (t_list)malloc(sizeof(t_list));
+	content = (t_list *)malloc(sizeof(t_list));
 	return (content);
 }
---
+++ training/linked.c
@@ -0,0 +1,5 @@
+typedef struct node
+{
+	int			val;
+	struct note	*next;
+}				node_t;
