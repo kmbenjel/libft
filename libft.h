2a7d0558cdf45771bfc708b664e83a0c50c9de6a
Khalid Benjelloun <khalidmailing@gmail.com>
2022-12-06 16:40:03 +0100

    kbenjell

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 20:07:41 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/12/06 16:08:52 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/12/06 16:39:21 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -56,11 +56,12 @@ void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
-//int					ft_lstsize(int c);
-//int					ft_lstlast(int c);
-//int					ft_lstadd_back(int c);
-//int					ft_lstdelone(int c);
-//int					ft_lstclear(int c);
-//int					ft_lstiter(int c);
-//int					ft_lstmap(int c);
+int					ft_lstsize(t_list, *lst);
+t_list				ft_lstlast(t_list, *lst);
+void				ft_lstadd_back(t_list, **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
 #endif
