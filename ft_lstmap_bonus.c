d3d3b8ffc84018485c93012d21d66f00c7bd68ea
Khalid Benjelloun <khalidmailing@gmail.com>
2022-12-07 11:21:00 +0100

    kbenjell

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 09:18:01 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/12/07 11:18:13 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/12/07 11:19:59 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,23 +15,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*tmp;
-	t_list	*curr;
 
-	curr = lst;
 	new = NULL;
-	if (!lst || !f)
-		return (new);
-	while (curr)
+	while (lst)
 	{
-		tmp = ft_lstnew(f(curr->content));
+		tmp = ft_lstnew(f(lst->content));
 		if (!tmp)
 		{
 			ft_lstclear(&new, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new, tmp);
-		curr = curr->next;
+		lst = lst->next;
 	}
-	ft_lstclear(&lst, del);
 	return (new);
 }
