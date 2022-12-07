7a6deeeaf2a25534f45b13676edd9d10b794b9c2
Khalid Benjelloun <khalidmailing@gmail.com>
2022-12-07 04:56:15 +0100

    kbenjell

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 16:36:01 by kbenjell          #+#    #+#             */
-/*   Updated: 2022/12/07 04:34:03 by kbenjell         ###   ########.fr       */
+/*   Updated: 2022/12/07 04:56:04 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,12 +15,12 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*current;
 
-	current = *lst;
-	while (current)
+	current = ft_lstlast(*lst);
+	while (current != *lst)
 	{
 		ft_lstdelone(current, del);
-		if (current->next)
-			current = current->next;
+		current = ft_lstlast(*lst);
 	}
+	//	del(*lst->content);
 	*lst = NULL;
 }
